19ea4b4d7d69f8f63f9df2a600f088140dcd98e4
Gallahaad <professional.utkarshmisra@gmail.com>
2023-11-16 04:09:48 +0530

    Complete Hoobank Web Application

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "./style";
+import {
+  Navbar,
+  Hero,
+  Footer,
+  Billing,
+  Business,
+  Button,
+  Carddeal,
+  Clients,
+  CTA,
+  FeedbackCard,
+  GetStarted,
+  Stats,
+  Testimonials,
+} from "./components";
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <Carddeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import Carddeal from "./Carddeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import GetStarted from "./GetStarted";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+  Navbar,
+  Hero,
+  Footer,
+  Billing,
+  Business,
+  Button,
+  Carddeal,
+  Clients,
+  CTA,
+  FeedbackCard,
+  GetStarted,
+  Stats,
+  Testimonials,
+};
